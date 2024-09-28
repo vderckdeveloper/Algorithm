912238716f8b24571721e50c67e4923985ac717b
vderckdeveloper <vderckdeveloper@gmail.com>
2024-09-28 15:40:42 +0900

    feat: divisor complete algorithm

--- diviosrComplete.js
+++ diviosrComplete.js
@@ -3,7 +3,12 @@ const divisor = 5;
 
 function solution(arr, divisor) {
     const answer = [];
+    // if remainder is zero, insert each element
     arr.forEach(item => item % divisor === 0 && answer.push(item));
+    // sort array in aescending order
+    answer.sort((a, b) => a - b);
+    // if array is empty, insert -1
+    if (answer.length === 0) answer.push(-1);
     return answer;
 }
 
