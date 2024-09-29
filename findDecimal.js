7ef6a0592af7025e678345c77e7ead480640d989
vderckdeveloper <vderckdeveloper@gmail.com>
2024-09-29 12:33:22 +0900

    refractor: optimize the algorithm

--- findDecimal.js
+++ findDecimal.js
@@ -5,21 +5,23 @@ function solution(n) {
     if (n < 2 || n > 1000000) return;
 
     const decimalList = [];
+    const isPrime = Array(n + 1).fill(true);
+    isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아님
 
-    // number to divide
-    for (let i = 2; i < n + 1; i++) {
-
-        let dividerCount = 0;
-        // number to become a divider
-        for (let j = 2; j < n + 1; j++) {
-            if (i !== j && i % j === 0) {
-                dividerCount++;
+    // Sieve of Eratosthenes
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
-
-        if (dividerCount === 0) decimalList.push(i); 
     }
-    
+
+    // Collect prime numbers
+    for (let i = 2; i <= n; i++) {
+        if (isPrime[i]) decimalList.push(i);
+    }
+
     const answer = decimalList.length;
     return answer;
 }
